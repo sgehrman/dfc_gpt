8ffef6169036a593087b5732f0601732fbf6d965
Steve Gehrman <sgehrman@gmail.com>
2023-08-21 03:31:34 -0700

    sdf

--- cppLib/build.sh
+++ cppLib/build.sh
@@ -5,7 +5,7 @@ libs="/home/steve/Documents/GitHub/dfc/dfc_gpt/cppLib/chatlib/gpt4all/gpt4all-ba
 dic="/home/steve/Documents/GitHub/dfc/dfc_gpt/cppLib/include/dart_api_dl.c"
 
 # make shared lib
-clang++  -fsanitize=memory -fno-omit-frame-pointer -g -O2 -shared -o dfc-gpt.so main.cpp $libs/libllmodel.so -x c $dic -Wl,-rpath,"$libs" -fPIC
+clang++ -shared -o dfc-gpt.so main.cpp $libs/libllmodel.so -x c $dic -Wl,-rpath,"$libs" -fPIC
 
 cp ./dfc-gpt.so /home/steve/.local/share/re.distantfutu.deckr/gpt/libs/
 
