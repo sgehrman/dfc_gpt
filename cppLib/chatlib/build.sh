d16fa2ba198b095828109e8c9a7da4cd446798d1
Steve Gehrman <sgehrman@gmail.com>
2023-08-25 08:38:44 -0700

    sdf

--- cppLib/chatlib/build.sh
+++ cppLib/chatlib/build.sh
@@ -1,6 +1,6 @@
 pushd "chatlib"
 
-# git clone --recurse-submodules https://github.com/nomic-ai/gpt4all
+git clone --recurse-submodules https://github.com/nomic-ai/gpt4all
 
 cd gpt4all/gpt4all-backend/
 mkdir build
