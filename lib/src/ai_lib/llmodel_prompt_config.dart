fd6b3f4bdac2670f6b426c8b2a13dde5c41fc1b4
Steve Gehrman <sgehrman@gmail.com>
2023-08-30 18:28:20 -0700

    sdf

--- lib/src/ai_lib/llmodel_prompt_config.dart
+++ lib/src/ai_lib/llmodel_prompt_config.dart
@@ -15,14 +15,26 @@ class LLModelPromptConfig {
   // double contextErase = 0.5;
 
   // ** original defaults
+  // int nPast = 0;
+  // int nCtx = 1024;
+  // int nPredict = 128;
+  // int topK = 40;
+  // double topP = 0.95;
+  // double temp = 0.28;
+  // int nBatch = 8;
+  // double repeatPenalty = 1.1;
+  // int repeatLastN = 10;
+  // double contextErase = 0.55;
+
+  // ** chat app defaults
   int nPast = 0;
   int nCtx = 1024;
-  int nPredict = 128;
+  int nPredict = 4096; // max length
   int topK = 40;
-  double topP = 0.95;
-  double temp = 0.28;
-  int nBatch = 8;
-  double repeatPenalty = 1.1;
-  int repeatLastN = 10;
+  double topP = 0.4;
+  double temp = 0.7;
+  int nBatch = 128; // prompt Batch Size
+  double repeatPenalty = 1.18;
+  int repeatLastN = 64; // repeat_penalty_tokens
   double contextErase = 0.55;
 }
