1203a601dd3d0a89370c70c61fac36dfc4d09236
Steve Gehrman <sgehrman@gmail.com>
2023-09-01 03:06:13 -0700

    sdf

--- macos/Classes/DfcGptPlugin.swift
+++ macos/Classes/DfcGptPlugin.swift
@@ -3,6 +3,6 @@ import Foundation
 
 public class DfcGptPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    print('register MacOSPlugin')
+    print("DfcGptPlugin")
    }
 }
