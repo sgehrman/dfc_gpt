2b5986acd94d9ecbe21b95ef100b3fa17f6f07a6
Steve Gehrman <sgehrman@gmail.com>
2024-04-12 11:39:34 +0900

    laptop fix

--- tools/build-lib.sh
+++ tools/build-lib.sh
@@ -5,6 +5,12 @@ pushd "shared_libs/dfc_gpt"
 rm -rf build
 mkdir build
 cd build
+
+# added to compile on laptop, see also ./shared_libs/dfc_gpt/CMakeLists.txt - needed for atomic
+# $ sudo apt install gcc-12
+export CC=/usr/bin/gcc-12
+export CXX=/usr/bin/g++-12
+
 cmake ..
 cmake --build . --parallel --config Release
 
